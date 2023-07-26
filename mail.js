d1a299b8eafc036c866cfbb38acdfda8bf3bbf41
Dom3390 <133919358+Dom3390@users.noreply.github.com>
2023-07-26 00:14:52 -0400

    Create mail.js

---
+++ mail.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAfZbmPrU68Ud-j_84Ha8xm-Qi9oBJCTQE",
+    authDomain: "my-landing-page-819b9.firebaseapp.com",
+    databaseURL: "https://my-landing-page-819b9-default-rtdb.firebaseio.com",
+    projectId: "my-landing-page-819b9",
+    storageBucket: "my-landing-page-819b9.appspot.com",
+    messagingSenderId: "6339012929",
+    appId: "1:6339012929:web:be0a56e091b5292a7199c0",
+    measurementId: "G-JW2Q1QKPZ2"
+  };
