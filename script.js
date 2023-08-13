b137cdc52ab2614852e06147c4fec847e408f89c
Dom3390 <133919358+Dom3390@users.noreply.github.com>
2023-08-13 19:47:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,33 @@ const observer = new IntersectionObserver(entries => {
 items.forEach(item => {
   observer.observe(item);
 })
+
+const sortSelect = document.getElementById('sortSelect');
+const sortableList = document.getElementById('sortable-list');
+
+// Sort the list based on user selection
+sortSelect.addEventListener('change', () => {
+    const sortOrder = sortSelect.value;
+    sortList(sortOrder);
+});
+
+function sortList(order) {
+    const items = Array.from(sortableList.children);
+    const sortedItems = items.sort((a, b) => {
+        const aValue = a.textContent.trim();
+        const bValue = b.textContent.trim();
+
+        if (order === 'asc') {
+            return aValue.localeCompare(bValue);
+        } else if (order === 'desc') {
+            return bValue.localeCompare(aValue);
+        }
+    });
+
+    sortableList.innerHTML = '';
+    sortedItems.forEach(item => sortableList.appendChild(item));
+}
+
+// Initialize with default ascending sort
+sortList('asc');
+
